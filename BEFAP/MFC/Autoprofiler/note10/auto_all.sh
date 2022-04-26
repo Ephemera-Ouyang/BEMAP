85b7aabdc08202e2147afd8e8229f0852ea36e64
Ephemera-Ouyang <101923263+Ephemera-Ouyang@users.noreply.github.com>
2022-04-26 15:30:58 +0800

    Add files via upload

--- BEFAP/MFC/Autoprofiler/note10/auto_all.sh
+++ BEFAP/MFC/Autoprofiler/note10/auto_all.sh
@@ -13,14 +13,14 @@
 app_name_list=("Alipay" "article" "autonavi" "baidu" "beike" "booking" "ctrip" "didi" "health" "homelink" "hwireader" "jingdong" "mediacenter" "mm" "netease" "pinduoduo" "qiyi" "qqlive" "qqmusic" "ss" "tmall" "UCMobile" "umetrip" "weibo" "ximalaya" "zhihu" "bili" "duapp" "gifmaker" "keep" "kiwi" "liulishuo" "meituan" "mooc" "reader" "taobao" "tieba" "xhs" "mtxx" "wiz" "coolapk" "fenbi" "netdisk" "wps" "xueersi" "xunlei" "yinxiang" "popcap" "AndroidAnimal" "pubgmhd" "antutu" "ludashi" "dianping" "autohome" "dragon" "ele" "cmb" "douyu" "jiayuan" "qqdownload" "qqmail" "mail" "qgame" "kuaikan" "video" "dwrg" "miniworld" "snake" "onmyoji" "sgame" "snapchat" "facebook" "messenger" "telegram" "amazonshop" "amazonmusic"
 "dealspure" "twitch" "facebookgaming" "kindle" "youtube" "twitter" "instagram" "playgame" "googlemap" "calm" "bigolive" "carousell" "starmaker" "tango" "webtoon" "googledrive" "onedrive" "uplive" "joox" "easymoney" "shein" "reddit" "tubitv" "outlook")
 
- do_name_list=("sliding" "switching" "quenching")
+do_name_list=("sliding" "switching" "quenching")
 
- for i in ${do_name_list[@]};
- do export do_name=$i;
+for i in ${do_name_list[@]};
+do export do_name=$i;
 
- export app_name=${app_name_list[0]}
+export app_name=${app_name_list[0]}
 
- sleep 10
- bash auto_test.sh;
- done
+sleep 10
+bash auto_test.sh;
+done
 
