26fbc9848147516d421aed04b52cf8ca5087f4df
Ephemera-Ouyang <101923263+Ephemera-Ouyang@users.noreply.github.com>
2024-06-21 15:50:03 +0800

    Add files via upload

---
+++ BEMAP/SPBench/switching_book.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# ---------------------------------------------------------------------
+# Mate 30 change script.
+# ---------------------------------------------------------------------
+#
+
+echo "The change.sh PID is :"$$
+sleep 1
+while [ $(ps -ef | grep -c "auto_test.sh") -gt 1 ]
+do
+   #adb shell input keyevent 61
+   adb shell input tap 750 170
+   sleep 3
+   adb shell input tap 95 170
+   sleep 3
+   adb shell input tap 95 170
+   sleep 3
+   adb shell input tap 95 170
+   sleep 3
+done
+
